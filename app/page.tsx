5e6128e198aad4bc0cec47c1c7bef7315be5db92
fazeleh shamsi <fazelehshamsi1380@gmail.com>
2024-12-06 17:44:32 +0330

    added api

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
+"use client"
+
 import Image from "next/image";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider ,useQuery} from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
 
 
 const queryClient = new QueryClient()
 
-const App = ({Component , pageProps}: AppProps) => {
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps}  />
+      <Example  />
     </QueryClientProvider>
+  )
+}
+
+function Example() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://rasadent-webservice-userpanel.liara.run/api/categories').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      <h1>{JSON.stringify(data.data)}</h1>
+    </div>
   )
 }
 
